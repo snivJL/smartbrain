4bf9e228f69ef63978e2e0b769ee7e5662fac3ce
snivJL <julien.lejay@gmail.com>
2020-05-01 19:13:00 +0700

    Updates

---
+++ precache-manifest.21986d67bf512135e761afee695395a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a7471f2d7799891153727866e55afc",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "c72359ae088eb565e491",
+    "url": "/smartbrain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "261832a588d48944b1d8",
+    "url": "/smartbrain/static/css/main.8480dab5.chunk.css"
+  },
+  {
+    "revision": "c72359ae088eb565e491",
+    "url": "/smartbrain/static/js/2.2e872ff4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smartbrain/static/js/2.2e872ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261832a588d48944b1d8",
+    "url": "/smartbrain/static/js/main.17eb0e36.chunk.js"
+  },
+  {
+    "revision": "e65e821cd885235ea5fc",
+    "url": "/smartbrain/static/js/runtime-main.54042368.js"
+  }
+]);
